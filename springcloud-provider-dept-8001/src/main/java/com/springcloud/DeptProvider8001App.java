b59e7ca1506d4a83eff4b3cadc3db7febf2dee90
tom_glb <tom@sina.com>
2019-09-24 23:10:53 +0800

    add eureka regist and instance-id,info message;

--- springcloud-provider-dept-8001/src/main/java/com/springcloud/DeptProvider8001App.java
+++ springcloud-provider-dept-8001/src/main/java/com/springcloud/DeptProvider8001App.java
@@ -2,6 +2,7 @@ package com.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Tengxq
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @description
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class DeptProvider8001App {
 
     public static void main(String[] args) {
