5c0ee8233fcac323905326131488774d94bddc58
tom_glb <tom@sina.com>
2019-09-25 23:34:58 +0800

    finished trunking system

--- springcloud-provider-dept-8001/src/main/java/com/springcloud/controller/DeptController.java
+++ springcloud-provider-dept-8001/src/main/java/com/springcloud/controller/DeptController.java
@@ -3,6 +3,8 @@ package com.springcloud.controller;
 import com.springcloud.entity.Dept;
 import com.springcloud.service.DeptService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,6 +19,9 @@ public class DeptController {
 
     @Autowired
     private DeptService deptService;
+
+    @Autowired
+    private DiscoveryClient client;
 
 
     @RequestMapping(value = "/dept/add",method = RequestMethod.POST)
@@ -33,6 +38,16 @@ public class DeptController {
         return deptService.findAll();
     }
 
+    @RequestMapping("/dept/discovery")
+    public Object discovery(){
+        List<String> list = client.getServices();
+        System.out.println("**************"+list);
 
+        List<ServiceInstance> srvList = client.getInstances("provider-dept-8001");
+        for (ServiceInstance element : srvList){
+            System.out.println(element.getServiceId()+"\t"+ element.getHost() + "\t" + element.getPort() + "\t" + element.getUri());
+        }
+        return this.client;
+    }
 
 }
